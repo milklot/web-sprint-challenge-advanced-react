5b526e91fa354cc527951152dc86d226d3586126
milklot <vbiostflex@gmail.com>
2021-02-21 19:20:40 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 function App() {
   // array of plants that have been added to the cart
   const [cart, setCart] = useState([]);
-
+  const initial = "initial";
   // add a plant to the cart
   const addToCart = (plant) => {
     setCart([...cart, plant]);
